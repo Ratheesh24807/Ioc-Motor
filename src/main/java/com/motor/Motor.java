b0bc37be47ff732273c5f4a486ab91c016441016
Ratheesh <ratheeshagowda@gmail.com>
2023-09-10 06:30:56 +0530

    intial commit

---
+++ src/main/java/com/motor/Motor.java
@@ -0,0 +1,24 @@
+package com.motor;
+
+public class Motor {
+
+	public Motor() {
+		System.out.println("Motor :: constructor");
+	}
+
+	public void start() {
+		System.out.println("Motor started");
+
+	}
+
+	public void dowork() {
+		System.out.println("motor pulling the water");
+
+	}
+
+	public void stop() {
+		System.out.println("Motor stopped");
+
+	}
+
+}
